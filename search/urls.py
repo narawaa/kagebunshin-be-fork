c788971f8d1c1750f36d1be2f6083abed3bd7cfb
halizaarfa <halizansa@gmail.com>
2025-12-04 22:46:04 +0700

    feat: basic get by type, first attempt of reranked query

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from search.views import get_data
+from search.views import *
 
 app_name = 'search'
 urlpatterns = [
     path('get-data/', get_data, name='get_example_data'),
+
+    path('anime/', get_anime, name='get_anime'),
+    path('anime/genre/', get_anime, name='get_anime_by_genre'),
+    path('character/', get_character, name='get_character'),
+
+    path('anime/query/', query_anime, name='query_anime'),
+    path('character/query/', query_character, name='query_character'),
 ]
